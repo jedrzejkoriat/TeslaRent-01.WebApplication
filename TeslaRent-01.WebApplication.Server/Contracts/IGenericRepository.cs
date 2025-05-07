c806ca6f704de46c84dec88552d305b1c4fdba85
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-05-08 01:25:36 +0200

    Add: generic repository with all needed methods (Add, Get, GetAll, Exists)

---
+++ TeslaRent-01.WebApplication.Server/Contracts/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TeslaRent_01.WebApplication.Server.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> AddAsync(T entity);
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<bool> ExistsAsync(int id);
+    }
+}
---
+++ TeslaRent-01.WebApplication.Server/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeslaRent_01.WebApplication.Server.Contracts;
+using TeslaRent_01.WebApplication.Server.Data;
+
+namespace TeslaRent_01.WebApplication.Server.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AppDbContext context;
+
+        public GenericRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await context.Set<T>().AnyAsync(e => EF.Property<int>(e, "Id") == id);
+        }
+    }
+}
