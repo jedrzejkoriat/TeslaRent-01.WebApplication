dacfeddf68cf42f1fe1a63015123a61d9191cad7
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-05-07 19:17:45 +0200

    Add: AppDbContext

---
+++ TeslaRent-01.WebApplication.Server/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeslaRent_01.WebApplication.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
