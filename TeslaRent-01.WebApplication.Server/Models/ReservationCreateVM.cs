11977b3be79bd9df75046fae73e52f389f2ced67
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-05-08 23:56:18 +0200

    Add: Validation in ReservationSearchVM

--- TeslaRent-01.WebApplication.Server/Models/ReservationCreateVM.cs
+++ TeslaRent-01.WebApplication.Server/Models/ReservationCreateVM.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace TeslaRent_01.WebApplication.Server.Models
 {
@@ -14,5 +15,13 @@ namespace TeslaRent_01.WebApplication.Server.Models
         public string Email { get; set; }
         [JsonPropertyName("price")]
         public decimal Price { get; set; }
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            foreach (var result in base.Validate(validationContext))
+            {
+                yield return result;
+            }
+        }
     }
 }
--- TeslaRent-01.WebApplication.Server/Models/ReservationSearchVM.cs
+++ TeslaRent-01.WebApplication.Server/Models/ReservationSearchVM.cs
@@ -1,16 +1,34 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace TeslaRent_01.WebApplication.Server.Models
 {
-    public class ReservationSearchVM
+    public class ReservationSearchVM : IValidatableObject
     {
+        [Required]
         [JsonPropertyName("startLocationId")]
         public int StartLocationId { get; set; }
+        [Required]
         [JsonPropertyName("endLocationId")]
         public int EndLocationId { get; set; }
+        [Required]
         [JsonPropertyName("startDate")]
         public DateTime StartDate { get; set; }
+        [Required]
         [JsonPropertyName("endDate")]
         public DateTime EndDate { get; set; }
+
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (StartDate.Date < DateTime.UtcNow.Date)
+            {
+                yield return new ValidationResult("Start date cannot be in the past.", new[] { nameof(StartDate) });
+            }
+
+            if (EndDate.Date < StartDate.Date)
+            {
+                yield return new ValidationResult("End date must be the same or after the start date.", new[] { nameof(EndDate) });
+            }
+        }
     }
 }
